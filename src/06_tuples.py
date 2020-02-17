85a874606947513eebfd73e1477d26c71536a41a
unknown <neduvinci@gmail.com>
2020-02-17 10:32:36 +0100

    edited tuples

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -45,6 +45,6 @@ t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1,)  # What needs to be added to make this work?
-
+#u = (1)  What needs to be added to make this work?
+u = (1,) # The separating comma is an important part of the tuple even when there is only one element
 print_tuple(u)
