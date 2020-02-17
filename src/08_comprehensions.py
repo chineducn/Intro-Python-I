f9f6724956c666b699922b874726df5f15142a93
unknown <neduvinci@gmail.com>
2020-02-17 11:06:36 +0100

    completed comprehensions

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,19 +10,14 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
-for n in range(5):
-    y.append(n + 1)
+y = [n for n in range(1,6)]
 
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
-
-for n in range(10):
-    y.append(n**3)
+y = [n**3 for n in range(10)]
 
 print(y)
 
@@ -31,10 +26,8 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [n.upper() for n in a]
 
-for n in a:
-    y.append(n.upper())
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
